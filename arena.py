00c0f7646639e9ef310c2d77541d3f9efcc90797
MooseV2 <anthony@desouza.li>
2018-01-31 13:31:42 -0500

    Added gridlines to drawing window

--- arena.py
+++ arena.py
@@ -10,9 +10,21 @@ pyglet.resource.reindex()
 arena_window = pyglet.window.Window(1000, 1000)
 drawing_batch = pyglet.graphics.Batch()
 
+def draw_gridlines(size, batch, spacing):
+  width, height = size
+  for x in range(0, width, spacing):
+    batch.add(2, pyglet.gl.GL_LINES, None, 
+      ("v2f", (x, 0, x, width))
+    )
+  for y in range(0, height, spacing):
+    batch.add(2, pyglet.gl.GL_LINES, None, 
+      ("v2f", (0, y, height, y))
+    )
+draw_gridlines((arena_window.width, arena_window.height), drawing_batch, 100)
+
 @arena_window.event
 def on_draw():
-  arena_window.clear()  
+  arena_window.clear()
   drawing_batch.draw()
 
 def update(dt):
