a09666475bb9e3018457c5af283aa11ec329826a
patrick.andruszkiewicz@uoit.net <PatrickA94>
2018-03-19 09:30:59 -0400

    qlearn

--- qlearn.py
+++ qlearn.py
@@ -26,10 +26,6 @@ class Catch(object):
         # print("State: ",self.state)
         state = self.state
 
-        #Update move counter
-        # count = state[0][0]
-        # count +=1
-
         #Current Position
         pos = self._get_Current()
 
@@ -56,18 +52,22 @@ class Catch(object):
 
         if axis == 'x':
             if pos[0]==0 and action == -1 :
-                pos[0] +=1
+                # pos[0] +=1
+                pass
             elif pos[0] ==self.grid_size-1 and action == 1:
-                pos[0] -=1
+                # pos[0] -=1
+                pass
             elif action == -1:
                 pos[0] -=1
             else :
                 pos[0] +=1
         if axis == 'y' :
             if pos[1]==0  and action == -1:
-                pos[1]+=1
+                # pos[1]+=1
+                pass
             elif pos[1] ==self.grid_size-1 and action == 1:
-                pos[1]-=1
+                # pos[1]-=1
+                pass
             elif action == -1:
                 pos[1] -=1
             else:
@@ -85,14 +85,18 @@ class Catch(object):
 
     def _draw_state(self):
         im_size = (self.grid_size,)*2
-        # points = self.state[0][0]+1
+        points = self.targets
         state = self.state
         canvas = np.zeros(im_size)
 
-        for pos in state:
+        for cnt,pos in enumerate(state):
             # print(pos)
-            canvas[pos[0],pos[1]] = 1
+            if cnt ==0:
+                canvas[pos[0],pos[1]] = 1
+            else:
+                canvas[pos[0],pos[1]] = 0.5
 
+        # canvas[points[0],points[1]] = 3
 
         # canvas[state[0]-1,state[1]-1] = 1  # draw bot
         return canvas
@@ -100,14 +104,16 @@ class Catch(object):
     def _get_reward(self):
         count = len(self.state)
         pos = self.state[0]
-        if (pos[0] == self.targets[0] and pos[1] == self.targets[1]) or (pos[0] == self.targets[2] and pos[1] == self.targets[3])  :
+        if (pos[0] == self.targets[0] and pos[1] == self.targets[1]):# or (pos[0] == self.targets[2] and pos[1] == self.targets[3])  :
             print ("Found a target in {} moves".format(count))
             self.targets[4] -=1
             return 1
         elif ([pos[0],pos[1]] in pos ):
-            return -1
+            # print("On my tail")
+            return -0.05
         else:
-            return 0.01
+            # return 1
+            pass
 
     def _is_over(self):
         if self.targets[4] == 0 or len(self.state) >= 250:
@@ -178,7 +184,7 @@ class ExperienceReplay(object):
 
 if __name__ == "__main__":
     # parameters
-    epsilon = 1  # exploration
+    epsilon = 0.8  # exploration
     _epsilon = epsilon
     num_actions = 4  # [move_left, stay, move_right]
     epoch = 1000
@@ -192,9 +198,9 @@ if __name__ == "__main__":
     model.add(Dense(hidden_size, input_shape=(grid_size**2,), activation='relu'))
     model.add(Dense(hidden_size, activation='relu'))
     model.add(Dense(num_actions))
-    # model.compile(sgd(lr=.2), "mse")
-    model.compile(Adam(lr=0.001, beta_1=0.9, beta_2=0.999,
-     epsilon=1e-08, decay=0.0), "mse")
+    model.compile(sgd(lr=.2), "mse")
+    # model.compile(Adam(lr=0.01, beta_1=0.9, beta_2=0.999,
+    # epsilon=1e-08, decay=0.0), "mse")
 
     # If you want to continue training from a previous model, just uncomment the line bellow
     # model.load_weights("model.h5")
@@ -213,8 +219,8 @@ if __name__ == "__main__":
         game_over = False
         # get initial input
         input_t = env.observe()
-        # _epsilon = _epsilon-(epsilon/epoch) # linear
-        _epsilon = epsilon*pow(1-decay,e) # exponential
+        _epsilon = _epsilon-(epsilon/epoch) # linear
+        # _epsilon = epsilon*pow(1-decay,e) # exponential
 
         while not game_over:
 
@@ -243,7 +249,7 @@ if __name__ == "__main__":
 
         # Save trained model weights and architecture, this will be used by the visualization code
         if (e%100 ==0):
-            modelname = "model"+str(e)
+            modelname = "models/model"+str(e)
             model.save_weights(modelname+".h5", overwrite=True)
             with open(modelname+".json", "w") as outfile:
                 json.dump(model.to_json(), outfile)
